de089877cd0e9b9a5ff4d74fb724da03f8cfdbd4
MonteShaffer <monte.shaffer@gmail.com>
2020-10-05 17:12:30 -0700

    version 0.1.3

--- humanVerseWSU/R/functions-imdb.R
+++ humanVerseWSU/R/functions-imdb.R
@@ -5,10 +5,52 @@
 #' This reads in 23 compressed files that are about 56MB.
 #' It will use a bit of RAM to load these data.
 #'
+#' Short notation for `imdb.data$actors$all`.
+#'
+#' `$actors$all`         # 71341
+#' `$actors$popular50`   # 35245
+#' `$actors$gem50`       # 37997
+#' `$actors$headliners`  #  2407
+#' `$actors$top250`      #  3092
+#' `$actors$nm5000`      #  4968
+#' `$actors$tt5000`      #  5452
+#'
+#'
+#' `$movies$all`         # 74520
+#' `$movies$popular50`   #  5704    3
+#' `$movies$gem50`       #  5704    3
+#' `$movies$top250`      #   250
+#' `$movies$tt5000`      #  4942
+#'
+#'
+#' `$headliners$companies`   #     770
+#' `$headliners$directors`   #     606
+#' `$headliners$writers`     #     670
+#' `$headliners$actors`      #    2407
+#' `$glue$movies.creatives`  #  361077      3   # this table was used to build headliners with 15+ rule
+#'
+#'
+#' `$all.movies.creatives`           #  407481      4
+#' `$all.movies.companies`           #  120376      4
+#' `$all.movies.actors.characters`   #  901324      4
+#'
+#' <https://www.imdb.com/filmosearch/?sort=num_votes&explore=title_type&role=nm0000071>
+#'
+#' `$all.actors.rank`     #  1842305       3   # rank is order of search result
+#' `$all.actors.movies`   #   282062      11   # this contains the data from search
+#' `$all.actors.info`     #    71332      10   # this contains basic actor info from <https://www.imdb.com/name/nm0000071/>
+#'
+#'
+#'
 #' This data was pulled from \url{https://www.imdb.com} in September 2020.
 #'
 #' It contains several dataframes.
-#' See \url{http://md5.mshaffer.com/WSU_STATS419/01_imdb-monte.html"} for details.
+#' See \url{http://md5.mshaffer.com/WSU_STATS419/01_imdb-monte.html} for details.
+#'
+#' Roxygen or RStudio does not allow wildcard `*` in example? Bold?
+#' See \url{\url{http://md5.mshaffer.com/WSU_STATS419/01_imdb-monte.html}} for examples.
+#'
+#' Once loaded, a global object `imdb.data` contains the lists of data.
 #'
 #' @family IMDB
 #'
@@ -91,39 +133,75 @@ loadDataIMDB = function()
   .GlobalEnv$imdb.data = imdb;
   }
 
-IMDB.getMovieInfo = function(ttid, imdb=imdb.data)
-  {
 
+
+#' IMDB.getMovieInfoFromActorSearch
+#'
+#' @param ttid Film identifier `ttid`
+#' @param return.cols list of columns you want to display, by default all
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+#'
+#' @return dataframe of results
+#' @export
+IMDB.getMovieInfoFromActorSearch = function(ttid, return.cols=NULL, imdb=imdb.data$all.actors.movies)
+  {
+  info = stats::na.omit( imdb[imdb$ttid == ttid, ] ) ;
+  if(is.null(return.cols)) { info; } else { info[, return.cols]; }
   }
 
 
-IMDB.searchMovieName = function(str, imdb=imdb.data)
+#' IMDB.searchMovieTitle
+#'
+#' @param str Search string with basic wildcard `*` operator
+#' @param return.cols list of columns you want to display, by default all
+#'
+#' @param ignore.case Defaults to TRUE, matches "mont" and "Mont"
+#' @param perl Defaults to FALSE, convert string query to PERL regex?
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+#'
+#'
+#' @return dataframe of results
+#' @export
+IMDB.searchMovieTitle = function(str, return.cols=NULL, ignore.case=TRUE, perl=FALSE, imdb=imdb.data$all.actors.movies)
   {
-
-
+  grx = utils::glob2rx(str);  # https://stackoverflow.com/questions/5823503/
+  grx.grep = grep(grx,imdb$title, ignore.case=ignore.case, perl=perl);
+  rows = imdb[grx.grep, ];
+  if(is.null(return.cols)) { rows; } else { rows[, return.cols]; }
   }
 
-IMDB.getNameOfMovie = function(ttid, imdb=imdb.data)
-  {
 
-  }
-
-# IMDB.getPersonInfo("nm0000005");
-# IMDB.getPersonInfo("nm0000005", 1:4);
+#' IMDB.getPersonInfo
+#'
+#' @param nmid  Person identifier `nmid`
+#' @param return.cols list of columns you want to display, by default all
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+#'
+#' # dim(imdb.data$all.actors.info);  # 71332    10
+#'
+#' @return dataframe of results
+#' @export
 IMDB.getPersonInfo = function(nmid, return.cols=NULL, imdb=imdb.data$all.actors.info)
   {
   info = imdb[imdb$nmid == nmid, ];
   if(is.null(return.cols)) { info; } else { info[, return.cols]; }
   }
 
-# IMDB.getNameOfPerson("nm0000005");
-IMDB.getNameOfPerson = function(nmid)
-  {
-  row = IMDB.getPersonInfo(nmid);
-  row$name;
-  }
 
-# IMDB.getMoviesForPerson("nm0000005", 1:5);
+
+#' IMDB.getMoviesForPerson
+#'
+#' @param nmid  Person identifier `nmid`
+#' @param return.cols list of columns you want to display, by default all
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+#'
+#' dim(imdb.data$all.actors.info);     # 71332      10
+#' dim(imdb.data$all.actors.movies);   # 282062     11
+#' # glue table ::  nmid|ttid|rank ... rank from ActorSearchMovies
+#' dim(imdb$all.actors.rank);          # 1842305     3
+#'
+#' @return dataframe of results
+#' @export
 IMDB.getMoviesForPerson = function(nmid, return.cols=NULL, imdb=imdb.data)
   {
   # $all.actors.movies
@@ -132,26 +210,33 @@ IMDB.getMoviesForPerson = function(nmid, return.cols=NULL, imdb=imdb.data)
   # movies info, just a few cols ...
   info = imdb$all.actors.rank[imdb$all.actors.rank$nmid == nmid, ];
 
-  # character name here as well?
+  # character name here as well?  From a different dataset ...
   # https://www.datasciencemadesimple.com/join-in-r-merge-in-r/
   info.more = merge(info, imdb$all.actors.movies, by="ttid");
+
+  #characters = imdb$all.movies.actors.characters[
+   #                 imdb$all.movies.actors.characters$nmid == nmid,];
+
+  #info.even.more = merge(info.more, imdb$all.movies.actors.characters, by="nmid");
+
+  info.more = sortDataFrameByNumericColumns(info.more,"rank", "ASC");
 
   if(is.null(return.cols)) { info.more; } else { info.more[, return.cols]; }
   }
 
-
-# IMDB.searchPersonName("Den*Wash*", return.cols=1:4);
-# IMDB.searchPersonName("Robin*");
-# IMDB.searchPersonName("robin*");
-# IMDB.searchPersonName("robin*", ignore.case=FALSE);
-# IMDB.searchPersonName("*obin*");
-# IMDB.searchPersonName("*st*");
-# IMDB.searchPersonName("Sean*");
-# IMDB.searchPersonName("Sean*", return.cols=1:5);
-# table(IMDB.searchPersonName("Fran*O*", imdb=imdb.data$top250.actors.movies, ignore.case=FALSE)$name);
-# IMDB.searchPersonName("Frank Oz");
-
-IMDB.searchPersonName = function(str, ignore.case=TRUE, perl=FALSE, return.cols=NULL,  imdb=imdb.data$all.actors.info)
+#' IMDB.searchPersonName
+#'
+#' @param str Search string with basic wildcard `*` operator
+#' @param return.cols list of columns you want to display, by default all
+#'
+#' @param ignore.case Defaults to TRUE, matches "mont" and "Mont"
+#' @param perl Defaults to FALSE, convert string query to PERL regex?
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+#'
+#'
+#' @return dataframe of results
+#' @export
+IMDB.searchPersonName = function(str, return.cols=NULL, ignore.case=TRUE, perl=FALSE, imdb=imdb.data$all.actors.info)
   {
   grx = utils::glob2rx(str);  # https://stackoverflow.com/questions/5823503/
   grx.grep = grep(grx,imdb$name, ignore.case=ignore.case, perl=perl);
@@ -159,31 +244,47 @@ IMDB.searchPersonName = function(str, ignore.case=TRUE, perl=FALSE, return.cols=
   if(is.null(return.cols)) { rows; } else { rows[, return.cols]; }
   }
 
-# IMDB.genericSearch("*Kentucky*", "bio");
-# IMDB.genericSearch("*Kentucky*", "bio", return.cols=1:5);
-# IMDB.genericSearch("*Kentucky*", "born.where", return.cols=1:5);
-# IMDB.genericSearch("*Monte*", "name", return.cols=1:5);
-# IMDB.genericSearch("*Monte*", "bio", return.cols=1:5);
-# IMDB.genericSearch("*Montana*", "bio", return.cols=1:5);
-# IMDB.genericSearch("*Montana*", "born.where", return.cols=c(1:2,4,5) );
-# IMDB.genericSearch("*Seattle*", "born.where", return.cols=c(1:2,4,5) );
-IMDB.genericSearch = function(str, col.name, ignore.case=TRUE, perl=FALSE, return.cols=NULL,  imdb=imdb.data$all.actors.info)
+
+#' IMDB.genericSearch
+#'
+#' @param str Search string with basic wildcard `*` operator
+#' @param col.name The column to perform search on
+#' @param return.cols list of columns you want to display, by default all
+#'
+#' @param ignore.case Defaults to TRUE, matches "mont" and "Mont"
+#' @param perl Defaults to FALSE, convert string query to PERL regex?
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+
+#'
+#' @return dataframe of results
+#' @export
+IMDB.genericSearch = function(str, col.name, return.cols=NULL, ignore.case=TRUE, perl=FALSE, imdb=imdb.data$all.actors.info)
   {
   grx = utils::glob2rx(str);  # https://stackoverflow.com/questions/5823503/
   grx.grep = grep(grx,imdb[col.name][[1]], ignore.case=ignore.case, perl=perl);
   rows = imdb[grx.grep, ];
   if(is.null(return.cols)) { rows; } else { rows[, return.cols]; }
-
   }
 
 
-# IMDB.getUniqueCharactersForPerson("nm0000136"); # Johnny Depp
-# IMDB.getUniqueCharactersForPerson(IMDB.searchPersonName("Frank Oz")$nmid[1]);
 
-IMDB.getUniqueCharactersForPerson = function(nmid, imdb=imdb.data$top250.actors.movies )
+#' IMDB.getUniqueCharactersForPerson
+#'
+#' This is currently a function of `$all.movies.actors.characters`
+#'  and *NOT* `$all.actors.info`
+#'
+#' @param nmid  Person identifier `nmid`
+#' @param imdb If you want to apply this function to a different dataframe, it's possible
+#'
+#'
+#' @return dataframe of results
+#' @export
+IMDB.getUniqueCharactersForPerson = function(nmid, imdb=imdb.data$all.movies.actors.characters )
   {
   rows = imdb[imdb$nmid==nmid, ];
-  na.omit( unique(rows$character) );
+  characters = as.data.frame( table( stats::na.omit( rows$character ) ) )[,c(2,1)];
+      colnames(characters) = c("count","character");
+  sortDataFrameByNumericColumns(characters,"count");
   }
 
 
--- humanVerseWSU/R/globals.R
+++ humanVerseWSU/R/globals.R
@@ -2,3 +2,5 @@ utils::globalVariables(c("github.path", "local.path", "local.data.path"));
 
 utils::globalVariables(c("inflation.df"));
 
+utils::globalVariables(c("imdb.data"));
+
